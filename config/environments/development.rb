3115f404ce4333f1617862042c7e759a9199543b
Lucas Castro <castro.lucas@gmail.com>
2016-07-06 16:19:25 -0300

    Configuração para envio de emails do sorteio pelo Gmail.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -43,12 +43,12 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "",
-    port: 587,    
-    domain: "infoway-pi.com.br", # pode usar aqui outro domínio, sem problemas
-    authentication: "plain",
-    enable_starttls_auto: true,
-    user_name: "", 
-    password: ""
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['GMAIL_USERNAME'],
+    password:             ENV['GMAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
   }
 end
