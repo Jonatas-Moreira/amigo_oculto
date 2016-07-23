8198b72d3a7eba64cdf8a95c5ea9fe8b02ccb3e7
Lucas Castro <castro.lucas@gmail.com>
2016-07-23 12:00:58 -0300

    Corrigindo migration

--- db/migrate/20160709135852_add_sorteio_id_to_amigos.rb
+++ db/migrate/20160709135852_add_sorteio_id_to_amigos.rb
@@ -1,6 +1,5 @@
 class AddSorteioIdToAmigos < ActiveRecord::Migration[5.0]
   def change
     add_reference :amigos, :sorteio, foreign_key: true
-    add_column :amigos, :sorteio_id, :integer
   end
 end
